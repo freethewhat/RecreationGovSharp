53462b2dece1906f70c80edcd9fd15069c9f6a4a
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:53:51 -0700

    add organization

---
+++ RecreationGovSharp/Schemas/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Organization
+    {
+        public string OrgId { get; set; }
+        public string OrgName { get; set; }
+        public string OrgImageUrl { get; set; }
+        public string OrgUrlText { get; set; }
+        public string OrgUrlAddress { get; set; }
+        public string OrgType { get; set; }
+        public string OrgAbbrevName { get; set; }
+        public string OrgJurisdictionType { get; set; }
+        public string OrgParentId { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+    }
+}
