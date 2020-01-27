18a55952e3c474a2b1ea1aab7e503453c49185e0
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:33:34 -0700

    add facility rec area

---
+++ RecreationGovSharp/Schemas/FacilityRecArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class FacilityRecArea
+    {
+        public string RecAreaId { get; set; }
+        public string RecAreaName { get; set; }
+        public string ResourceLink { get; set; }
+    }
+}
