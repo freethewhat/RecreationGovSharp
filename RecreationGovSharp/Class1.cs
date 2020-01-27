21bcc1c87994e96db9bfc176d381d06e0eddec2d
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 15:40:16 -0700

    create webagent

--- RecreationGovSharp/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RecreationGovSharp
-{
-    public class Class1
-    {
-    }
-}
---
+++ RecreationGovSharp/WebAgent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security;
+using System.Text;
+
+namespace RecreationGovSharp
+{
+    public class WebAgent
+    {
+        private readonly string _baseUrl = "https://ridb.recreation.gov/api/v1/";
+
+        public SecureString ApiKey { get; set; }
+
+        public WebAgent()
+        {
+
+        }
+    }
+}
