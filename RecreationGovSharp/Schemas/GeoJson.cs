faf27f5ba00a9b8c694c5eac68d22edf11255cb7
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 09:26:55 -0700

    add GeoJson

---
+++ RecreationGovSharp/Schemas/GeoJson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class GeoJson
+    {
+        public string Type { get; set; }
+        public IList<double> Coordinates { get; set; }
+    }
+}
