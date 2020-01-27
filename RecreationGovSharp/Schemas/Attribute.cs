cd740de2294788a0220585fd897afb161c03d974
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 07:19:32 -0700

    Add attribute

---
+++ RecreationGovSharp/Schemas/Attribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Attribute
+    {
+        public int AttributeId { get; set; }
+        public string AttributeName { get; set; }
+        public string AttributeValue { get; set; }
+    }
+}
