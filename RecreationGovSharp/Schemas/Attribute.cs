c0910bc69e7213d286649fae4c753dd843a68cdd
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 13:22:45 -0700

    Add attribute property restrictions

--- RecreationGovSharp/Schemas/Attribute.cs
+++ RecreationGovSharp/Schemas/Attribute.cs
@@ -7,7 +7,32 @@ namespace RecreationGovSharp.Schemas
     public class Attribute
     {
         public int AttributeId { get; set; }
-        public string AttributeName { get; set; }
-        public string AttributeValue { get; set; }
+
+        private string _attribute;
+        public string AttributeName {
+            get
+            {
+                return _attribute;
+            }
+            set
+            {
+                if (value.Length > 60)
+                    throw new ArgumentOutOfRangeException("Attribute name cannot be longer than 60 characters.");
+
+                _attribute = value;
+            }
+        }
+
+        private string _attributeValue;
+        public string AttributeValue {
+            get { return _attributeValue; }
+            set
+            {
+                if (value.Length > 255)
+                    throw new ArgumentOutOfRangeException("Attribute value cannot be longer than 255 characters.");
+
+                _attributeValue = value;
+            }
+        }
     }
 }
