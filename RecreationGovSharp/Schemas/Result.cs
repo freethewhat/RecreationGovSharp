d9d6f65f20c3d0b8670660c84c1b1189b4113de9
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 11:00:09 -0700

    add result

---
+++ RecreationGovSharp/Schemas/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Result
+    {
+        public int CurrentCount { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
