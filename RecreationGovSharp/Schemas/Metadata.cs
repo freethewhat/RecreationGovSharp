691cb945da76e5f66c1960b83214830dfce38314
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 11:05:50 -0700

    add metadata

---
+++ RecreationGovSharp/Schemas/Metadata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Metadata
+    {
+        public Results Results { get; set; }
+        public Parameters Parameters { get; set; }
+    }
+}
