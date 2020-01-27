ff219b80a281cd2ec1cb29cf0bfd66f5981cec5f
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 07:18:27 -0700

    Add activity

---
+++ RecreationGovSharp/Schemas/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public int ActivityParentId { get; set; }
+        public string ActivityName { get; set; } // is this an enum. If so what are the values?
+        public int ActivityLevel { get; set; }
+    }
+}
