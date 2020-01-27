9eefeca9962ca2b3ba17e809f0f4205f51e04b82
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 09:23:10 -0700

    add permitted equipment

---
+++ RecreationGovSharp/Schemas/PermittedEquipment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class PermittedEquipment
+    {
+        public string EquipmentName { get; set; }
+        public double MaxLength { get; set; }
+    }
+}
