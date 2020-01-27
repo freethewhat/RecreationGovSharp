7c68ff84c68350ef84104bc7925d6e32b6d7d9a8
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:02:08 -0700

    Add facility activity

---
+++ RecreationGovSharp/Schemas/FacilityActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class FacilityActivity
+    {
+        public string ActivityId { get; set; }
+        public string FacilityId { get; set; }
+        public string ActivityName { get; set; }
+        public string FacilityActivityDescription { get; set; }
+        public string FacilityActivityFeeDescription { get; set; }
+    }
+}
