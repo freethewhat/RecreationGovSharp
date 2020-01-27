36ad653695667047a69470a66a351f0a94fddded
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 09:45:21 -0700

    add tour

---
+++ RecreationGovSharp/Schemas/Tour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Tour
+    {
+        public string TourId { get; set; }
+        public string FacilityId { get; set; }
+        public string TourName { get; set; }
+        public string TourType { get; set; }
+        public string TourDescription { get; set; }
+        public int TourDuration { get; set; }
+        public bool TourAccessible { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public IList<Attribute> Attributes { get; set; }
+        public IList<Media> EntityMedia { get; set; }
+        public IList<int> MemberTours { get; set; }
+    }
+}
