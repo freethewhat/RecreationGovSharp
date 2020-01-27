196a68e92f9293c8a656b8dc531f217d4ec9040d
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 09:35:55 -0700

    add recreation area activity

---
+++ RecreationGovSharp/Schemas/RecreationAreaActivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class RecreationAreaActivity
+    {
+        public string ActivityId { get; set; }
+        public string ActivityParentId { get; set; }
+        public string RecAreaId { get; set; }
+        public string ActivityName { get; set; }
+        public string RecAreaActivityDescription { get; set; }
+        public string RecAreaActivityFeeDescription { get; set; }
+    }
+}
