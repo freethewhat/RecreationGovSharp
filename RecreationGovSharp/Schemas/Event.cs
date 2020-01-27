32537f499d2d0bed331fedb26b278930c91c40d9
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 13:32:28 -0700

    Add event property restrictions

--- RecreationGovSharp/Schemas/Event.cs
+++ RecreationGovSharp/Schemas/Event.cs
@@ -7,7 +7,19 @@ namespace RecreationGovSharp.Schemas
     public class Event
     {
         public string EventId { get; set; }
-        public string EventName { get; set; }
+
+        private string _eventName;
+        public string EventName {
+            get { return _eventName; }
+            set
+            {
+                if (value.Length > 60)
+                    throw new ArgumentOutOfRangeException("Event name cannot be longer than 60 characters.");
+
+                _eventName = value;
+            }
+        } 
+
         public string ResourceLink { get; set; }
     }
 }
