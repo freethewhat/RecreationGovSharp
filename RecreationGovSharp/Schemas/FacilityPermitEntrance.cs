fe725c23b74142e61996fc741a33f0099920dc10
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:26:05 -0700

    add facility permis entrance

---
+++ RecreationGovSharp/Schemas/FacilityPermitEntrance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    class FacilityPermisEntrance
+    {
+        public string PermitEntranceId { get; set; }
+        public string PermitEntranceName { get; set; }
+        public string ResourceLink { get; set; }
+    }
+}
