51f45311f34acfc58c2e7c5af937ebf2740278f5
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 07:14:26 -0700

    Add project files.

---
+++ RecreationGovSharpTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RecreationGovSharpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
