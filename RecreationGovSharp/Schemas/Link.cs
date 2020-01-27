05e616fc6cedc0a8c1fbaf5e94399e8a7dfd7351
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:42:05 -0700

    add link

---
+++ RecreationGovSharp/Schemas/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Link
+    {
+        public string EntityLinkId { get; set; }
+        public string LinkType { get; set; }
+        public string EntityId { get; set; }
+        public string EntityType { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
