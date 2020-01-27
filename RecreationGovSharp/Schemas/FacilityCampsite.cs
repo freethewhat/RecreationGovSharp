ff3e2ef2c45c8fecd71e316926e6b9cdaed6b5ac
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:24:37 -0700

    Add facility campsite

---
+++ RecreationGovSharp/Schemas/FacilityCampsite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class FacilityCampsite
+    {
+        public string CampsiteId { get; set; }
+        public string CampsiteName { get; set; }
+        public string ResourceLink { get; set; }
+    }
+}
