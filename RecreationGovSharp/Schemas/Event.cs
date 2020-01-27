c93cc7029b045117bfd8da0238383b9e7d50dcd3
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 07:24:23 -0700

    Add Event

---
+++ RecreationGovSharp/Schemas/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Event
+    {
+        public string EventId { get; set; }
+        public string EventName { get; set; }
+        public string ResourceLink { get; set; }
+    }
+}
