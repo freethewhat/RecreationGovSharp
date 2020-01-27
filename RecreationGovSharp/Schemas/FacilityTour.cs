d34cd26f858b672d124d55c02d54e53add3b38c5
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 08:34:30 -0700

    add facility tour

---
+++ RecreationGovSharp/Schemas/FacilityTour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class FacilityTour
+    {
+        public string TourId { get; set; }
+        public string TourName { get; set; }
+        public string ResourceLink { get; set; }
+    }
+}
