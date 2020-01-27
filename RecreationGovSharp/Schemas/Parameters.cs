468ef058de836a630c5184917b4349fe677594c4
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 11:04:23 -0700

    add parameters

---
+++ RecreationGovSharp/Schemas/Parameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Parameters
+    {
+        public string Query { get; set; }
+        public int Limit { get; set; }
+        public int Offset { get; set; }
+    }
+}
