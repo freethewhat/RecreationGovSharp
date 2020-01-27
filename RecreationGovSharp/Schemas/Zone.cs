94e4dab2880ed051c7d0bffb88eab19a583fa253
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-27 09:46:02 -0700

    add zone

---
+++ RecreationGovSharp/Schemas/Zone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecreationGovSharp.Schemas
+{
+    public class Zone
+    {
+        public string PermitEntranceZoneId { get; set; }
+        public string Zone { get; set; }
+    }
+}
