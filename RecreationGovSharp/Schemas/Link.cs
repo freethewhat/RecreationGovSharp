3be1bb2ee2856a2bb36d0acdcaede6693fe56121
Matt Sutton <mcsutton@outlook.com>
2020-01-27 19:51:50 -0700

    add property length limitations

--- RecreationGovSharp/Schemas/Link.cs
+++ RecreationGovSharp/Schemas/Link.cs
@@ -7,11 +7,56 @@ namespace RecreationGovSharp.Schemas
     public class Link
     {
         public string EntityLinkId { get; set; }
-        public string LinkType { get; set; }
+        
+        private string _linkType;
+        public string LinkType { 
+            get { return _linkType; } 
+            set
+            {
+                if (value.Length > 500)
+                    throw new ArgumentOutOfRangeException("Link type cannot be longer than 500 characters.");
+
+                _linkType = value;
+            } 
+        } 
         public string EntityId { get; set; }
-        public string EntityType { get; set; }
-        public string Title { get; set; }
+        
+        private string _entityType;
+        public string EntityType { 
+            get { return _entityType; } 
+            set
+            {
+                if (value.Length > 50)
+                    throw new ArgumentOutOfRangeException("Entity type cannot be longer than 50 characters.");
+
+                _entityType = value;
+            } 
+        } 
+        
+        private string _title;
+        public string Title { 
+            get { return _title; } 
+            set
+            {
+                if (value.Length > 500)
+                    throw new ArgumentOutOfRangeException("Title cannot be longer than 500 characters.");
+
+                _title = value;
+            } 
+        } 
+
         public string Description { get; set; }
-        public string Url { get; set; }
+        
+        private string _url;
+        public string Url { 
+            get { return _url; } 
+            set
+            {
+                if (value.Length > 2000)
+                    throw new ArgumentOutOfRangeException("URL cannot be longer than 2000 characters.");
+
+                _url = value;
+            } 
+        } 
     }
 }
