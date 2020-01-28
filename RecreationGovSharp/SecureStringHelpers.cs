b8f89cb606039934b9667af03132eec18d1bb4e3
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-28 13:42:12 -0700

    add WebAgent and SecureString helpers

---
+++ RecreationGovSharp/SecureStringHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace RecreationGovSharp
+{
+    public static class SecureStringHelpers
+    {
+        public static string ToString(this SecureString secureString)
+        {
+            IntPtr stringPointer = Marshal.SecureStringToBSTR(secureString);
+            var password = Marshal.PtrToStringBSTR(stringPointer);
+            Marshal.ZeroFreeBSTR(stringPointer);
+            return password;
+        }
+
+        public static SecureString ToSecureString(this string str)
+        {
+            SecureString secureString = new SecureString();
+            
+            // Add the characters into the secure string.
+            for (int i = 0; i < str.Length; i++)
+            {
+                secureString.AppendChar(str[i]);
+            }
+            
+            str = null;
+            GC.Collect();
+
+            return secureString;
+        }
+    }
+}
--- RecreationGovSharp/WebAgent.cs
+++ RecreationGovSharp/WebAgent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Security;
 using System.Text;
@@ -11,10 +12,44 @@ namespace RecreationGovSharp
         private readonly string _baseUrl = "https://ridb.recreation.gov/api/v1/";
 
         public SecureString ApiKey { get; set; }
+        public HttpWebRequest Request { get; set; }
 
-        public WebAgent()
+        public WebAgent(SecureString apiKey)
         {
+            ApiKey = apiKey;
+        }
 
+        private HttpWebRequest CreateRequest(string url, string method)
+        {
+            HttpWebRequest request;
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.Method = method;
+
+            request.Headers.Add("apikey", ApiKey.ToString());
+            return request;
+        }
+
+        public WebAgent CreateGetRequest(string url)
+        {
+            Request = CreateRequest(url, "GET");
+            return this; 
+        }
+
+        public string GetResponse()
+        {
+            var response = (HttpWebResponse)Request.GetResponse();
+            string message;
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                message = streamReader.ReadToEnd();
+            }
+
+            return message;
         }
     }
 }
