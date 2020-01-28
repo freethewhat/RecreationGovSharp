15dc8d81028f0e506702f3f539cebfc8fcc30ec0
Matt Sutton <mcsutton@outlook.com>
2020-01-27 19:01:54 -0700

    Add facility activity property restrictions

--- RecreationGovSharp/Schemas/FacilityActivity.cs
+++ RecreationGovSharp/Schemas/FacilityActivity.cs
@@ -8,8 +8,41 @@ namespace RecreationGovSharp.Schemas
     {
         public string ActivityId { get; set; }
         public string FacilityId { get; set; }
-        public string ActivityName { get; set; }
-        public string FacilityActivityDescription { get; set; }
-        public string FacilityActivityFeeDescription { get; set; }
+
+        private string _activityName;
+        public string ActivityName { 
+            get { return _activityName; }
+            set
+            {
+                if (value.Length > 60)
+                    throw new ArgumentOutOfRangeException("Activity name cannot be longer than 60 characters.");
+
+                _activityName = value;
+            } 
+        } 
+
+        private string _facilityActivityDescription;
+        public string FacilityActivityDescription { 
+            get { return _facilityActivityDescription; } 
+            set 
+            {
+                if (value.Length > 1024)
+                    throw new ArgumentOutOfRangeException("Facility activity description cannot be longer than 1024 characters.");
+
+                _facilityActivityDescription = value;
+            } 
+        } 
+
+        private string _facilityActivityFeeDescription;
+        public string FacilityActivityFeeDescription { 
+            get { return _facilityActivityFeeDescription; } 
+            set 
+            {
+                if (value.Length > 1024)
+                    throw new ArgumentOutOfRangeException("Facility activity fee description cannot be longer than 1024 characters.");
+
+                _facilityActivityFeeDescription = value;
+            } 
+        } 
     }
 }
