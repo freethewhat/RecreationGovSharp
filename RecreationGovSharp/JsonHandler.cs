1a696007c9ce3f43cac3d1daf0d91846d7ace5d5
Matt Sutton <matt.sutton@askallegiance.com>
2020-01-28 15:01:59 -0700

    add json handler

---
+++ RecreationGovSharp/JsonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RecreationGovSharp
+{
+    public class JsonHandler<T> where T : class
+    {
+        private static JsonSerializerOptions GetSerializerOptions()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                IgnoreNullValues = true,
+                PropertyNameCaseInsensitive = true,
+            };
+
+            return options;
+        }
+
+        public static T Deserialize(string json)
+        {
+            var options = GetSerializerOptions();
+            var result = JsonSerializer.Deserialize<T>(json, options);
+            return result;
+        }
+    }
+}
